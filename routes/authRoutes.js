acb77752636877fdadbd724f334d9786050a3f7c
Jmoney <67088606+AsanroJay@users.noreply.github.com>
2025-10-15 00:05:53 +0800

    fix: admin route capitalization issue

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/',(req,res) => {
-    res.render('ADMIN/login')
+    res.render('Admin/login')
 });
 
 router.get('/logout', authController.logout);
